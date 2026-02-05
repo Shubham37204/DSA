d80056d7353867c9c67ec1a3a27b388e47e91260
shubham bhardwaj <87294776+Shubham37204@users.noreply.github.com>
2026-02-05 19:12:18 +0530

    Delete STRING directory

--- STRING/Remove_All_Spaces.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def removeWhitespaces(self, s):
-        return s.replace(" ", "")
-
-# Example usage:
-sol = Solution()
-s = "Hello World!"
-print(sol.removeWhitespaces(s))  # Output: "HelloWorld!"
